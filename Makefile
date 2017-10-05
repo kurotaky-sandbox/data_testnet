f6208cd9f3ef1df71fafa2f393c2c4cd5226c861
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-10-05 18:26:55 +0900

    KurotakiDenkiCoin

---
+++ Makefile
@@ -0,0 +1,6 @@
+name            data_testnet
+description
+version         0.0.1
+
+author          kurotaky
+license         Apache-2.0
---
+++ src/KurotakiDenkiCoin.sol
@@ -0,0 +1,29 @@
+pragma solidity  ^ 0.4.15;
+
+contract KurotakiDenkiCoin {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping (address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function KurotakiDenkiCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) {
+        balanceOf[msg.sender] = _supply;
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _supply;
+    }
+
+    function transfer(address _to, uint256 _value) {
+        if (balanceOf[msg.sender] < _value) revert();
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert();
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        Transfer(msg.sender, _to, _value);
+    }
+}
