03f7488d1ddc5df8a11c8f731ee5c32716d0a23c
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-10-10 21:31:49 +0900

    キャッシュバックを試した

--- OkashiKingdomCoin.sol
+++ OkashiKingdomCoin.sol
@@ -8,6 +8,7 @@ contract OkashiKingdomCoin {
     uint256 public totalSupply;
     mapping (address => uint256) public balanceOf;
     mapping (address => int8) public blackList;
+    mapping (address => int8) public cashbackRate;
     address public owner;
 
     modifier onlyOwner() { if (msg.sender != owner) revert(); _; }
@@ -17,6 +18,8 @@ contract OkashiKingdomCoin {
     event DeleteFromBlacklist(address indexed target);
     event RejectedPaymentToBlacklistedAddr(address indexed from, address indexed to, uint256 value);
     event RejectedPaymentFromBlacklistedAddr(address indexed from, address indexed to, uint256 value);
+    event SetCashback(address indexed addr, int8 rate);
+    event Cashback(address indexed from, address indexed to, uint256 value);
 
     function OkashiKingdomCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) public {
         balanceOf[msg.sender] = _supply;
@@ -37,6 +40,19 @@ contract OkashiKingdomCoin {
         DeleteFromBlacklist(_addr);
     }
 
+    function SetCashbackRate(int8 _rate) {
+        if (_rate < 1) {
+            _rate = -1;
+        } else if (_rate > 100) {
+            _rate = 100;
+        }
+        cashbackRate[msg.sender] = _rate;
+        if (_rate < 1) {
+            _rate = 0;
+        }
+        SetCashback(msg.sender, _rate);
+    }
+
     function transfer(address _to, uint256 _value) public {
         if (balanceOf[msg.sender] < _value) revert();
         if (balanceOf[_to] + _value < balanceOf[_to]) revert();
@@ -46,9 +62,14 @@ contract OkashiKingdomCoin {
         } else if (blackList[_to] > 0) {
             RejectedPaymentToBlacklistedAddr(msg.sender, _to, _value);
         } else {
+            uint256 cashback = 0;
+            if (cashbackRate[_to] > 0) cashback = _value / 100 * uint256(cashbackRate[_to]);
+
             balanceOf[msg.sender] -= _value;
             balanceOf[_to] += _value;
+
             Transfer(msg.sender, _to, _value);
+            Cashback(_to, msg.sender, cashback);
         }
     }
 }
