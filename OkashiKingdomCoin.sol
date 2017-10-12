c1d62aac53f18765845c8a4e2871f6a7a9ac4819
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-10-12 18:59:39 +0900

    会員管理機能追加

--- OkashiKingdomCoin.sol
+++ OkashiKingdomCoin.sol
@@ -1,24 +1,101 @@
 pragma solidity  ^ 0.4.15;
 
-// ブラックリスト機能つき
-contract OkashiKingdomCoin {
+contract Owned {
+    // 状態変数
+    address public owner;
+
+    event TransferOwnership(address oldaddr, address newaddr);
+
+    modifier onlyOwner() { if (msg.sender != owner) revert(); _; }
+
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    function transferOwnership(address _new) onlyOwner {
+        address oldaddr = owner;
+        owner = _new;
+        TransferOwnership(oldaddr, owner);
+    }
+}
+
+contract Members is Owned {
+    address public coin;
+    MemberStatus[] public status;
+    mapping(address => History) public tradingHistory;
+
+    struct MemberStatus {
+        string name;
+        uint256 times;
+        uint256 sum;
+        int8 rate;
+    }
+
+    struct History {
+        uint256 times;
+        uint256 sum;
+        uint256 statusIndex;
+    }
+
+    modifier onlyCoin() { if (msg.sender == coin) _; }
+
+    function setCoin(address _addr) onlyOwner {
+        coin = _addr;
+    }
+
+    function pushStatus(string _name, uint256 _times, uint256 _sum, int8 _rate) onlyOwner {
+        status.push(MemberStatus({
+            name: _name,
+            times: _times,
+            sum: _sum,
+            rate: _rate
+        }));
+    }
+
+    function editStatus(uint256 _index, string _name, uint256 _times, uint256 _sum, int8 _rate) onlyOwner {
+        if (_index < status.length) {
+            status[_index].name = _name;
+            status[_index].times = _times;
+            status[_index].sum = _sum;
+            status[_index].rate = _rate;
+        }
+    }
+
+    function updateHistory(address _member, uint256 _value) onlyCoin {
+        tradingHistory[_member].times += 1;
+        tradingHistory[_member].sum += _value;
+
+        uint256 index;
+        int8 tmprate;
+        for(uint i = 0; i < status.length; i++) {
+            if (tradingHistory[_member].times >= status[i].times && tradingHistory[_member].sum >= status[i].sum && tmprate < status[i].rate) {
+                index = i;
+                tmprate = status[i].rate;
+            }
+        }
+        tradingHistory[_member].statusIndex = index;
+    }
+
+    function getCashbackRate(address _member) constant returns (int8 rate) {
+        rate = status[tradingHistory[_member].statusIndex].rate;
+    }
+}
+
+contract OkashiKingdomCoin is Owned {
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
+
     mapping (address => uint256) public balanceOf;
     mapping (address => int8) public blackList;
-    mapping (address => int8) public cashbackRate;
-    address public owner;
-
-    modifier onlyOwner() { if (msg.sender != owner) revert(); _; }
+    mapping (address => Members) public members;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Blacklisted(address indexed target);
     event DeleteFromBlacklist(address indexed target);
     event RejectedPaymentToBlacklistedAddr(address indexed from, address indexed to, uint256 value);
     event RejectedPaymentFromBlacklistedAddr(address indexed from, address indexed to, uint256 value);
-    event SetCashback(address indexed addr, int8 rate);
     event Cashback(address indexed from, address indexed to, uint256 value);
 
     function OkashiKingdomCoin(uint256 _supply, string _name, string _symbol, uint8 _decimals) public {
@@ -27,7 +104,6 @@ contract OkashiKingdomCoin {
         symbol = _symbol;
         decimals = _decimals;
         totalSupply = _supply;
-        owner = msg.sender;
     }
 
     function blacklisting(address _addr) onlyOwner {
@@ -40,17 +116,8 @@ contract OkashiKingdomCoin {
         DeleteFromBlacklist(_addr);
     }
 
-    function SetCashbackRate(int8 _rate) {
-        if (_rate < 1) {
-            _rate = -1;
-        } else if (_rate > 100) {
-            _rate = 100;
-        }
-        cashbackRate[msg.sender] = _rate;
-        if (_rate < 1) {
-            _rate = 0;
-        }
-        SetCashback(msg.sender, _rate);
+    function setMembers(Members _members) {
+        members[msg.sender] = Members(_members);
     }
 
     function transfer(address _to, uint256 _value) public {
@@ -63,10 +130,13 @@ contract OkashiKingdomCoin {
             RejectedPaymentToBlacklistedAddr(msg.sender, _to, _value);
         } else {
             uint256 cashback = 0;
-            if (cashbackRate[_to] > 0) cashback = _value / 100 * uint256(cashbackRate[_to]);
+            if (members[_to] > address(0)) {
+                cashback = _value / 100 * uint256(members[_to].getCashbackRate(msg.sender));
+                members[_to].updateHistory(msg.sender, _value);
+            }
 
-            balanceOf[msg.sender] -= _value;
-            balanceOf[_to] += _value;
+            balanceOf[msg.sender] -= (_value - cashback);
+            balanceOf[_to] += (_value - cashback);
 
             Transfer(msg.sender, _to, _value);
             Cashback(_to, msg.sender, cashback);
